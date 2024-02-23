cf0d039f58574bb7c5d945657f5cbda5d934559f
Tim <drum831@gmail.com>
2024-02-24 00:19:45 +0800

    feat: 新增 products 資料表欄位, 在前台顯示時呈現中文, 並調整產品 index 畫面

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace productSystem.Models
 {
@@ -11,17 +12,25 @@ namespace productSystem.Models
         }
 
         public int ProductId { get; set; }
+        [Display(Name = "產品名稱")]
         public string ProductName { get; set; } = null!;
         public int? SupplierId { get; set; }
         public int? CategoryId { get; set; }
+        [Display(Name = "每單位數量")]
         public string? QuantityPerUnit { get; set; }
+        [Display(Name = "單位價格")]
         public decimal? UnitPrice { get; set; }
+        [Display(Name = "庫存量")]
         public short? UnitsInStock { get; set; }
+        [Display(Name = "訂貨量")]
         public short? UnitsOnOrder { get; set; }
         public short? ReorderLevel { get; set; }
+        [Display(Name = "停產")]
         public bool Discontinued { get; set; }
 
+        [Display(Name = "類別")]
         public virtual Category? Category { get; set; }
+        [Display(Name = "供應商")]
         public virtual Supplier? Supplier { get; set; }
         public virtual ICollection<OrderDetail> OrderDetails { get; set; }
     }
