bab5a771bb1eb469812166f047f4ff42d11c57bf
Tim <drum831@gmail.com>
2024-02-25 00:46:08 +0800

    refactor: 移除多餘代碼

--- Program.cs
+++ Program.cs
@@ -29,14 +29,6 @@ using (var serviceScope = app.Services.CreateScope())
     }
 }
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
